734c8036f15ed95c9ba6b1a624f03be2359dbbae
b0nsu <125778250+b0nsu@users.noreply.github.com>
2026-08-29 14:32:46 +0900

    feat(desktop): mount v0.3 bounded analysis bar

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AnalysisBar from "./AnalysisBar";
 import App from "./App";
 import "./tokens.css";
 import "./styles.css";
 import "./v21.css";
+import "./v03.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div className="desktop-root">
+      <AnalysisBar />
+      <App />
+    </div>
   </React.StrictMode>
 );
